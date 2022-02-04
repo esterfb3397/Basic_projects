e396ff1cd62fa60af883034b40b4d69191022091
Esther Fernandez <estherfb_3397@hotmail.com>
2022-02-04 19:10:15 +0100

    Make your own mad lib

---
+++ Basic_Projects/MAD_LIBS/short_story.py
@@ -0,0 +1,15 @@
+# f-string5
+
+Nombre_propio = input("Nombre propio: ")
+adjetivo1 = input("Adjetivo: ")
+Pronombre = input("Pronombre masculino o femenino: ")
+determinante1 = input("Determinante: ")
+sustantivo1 = input("Sustantivo: ")
+determinante2 = input("Determinante: ")
+sustantivo2 = input("Sustantivo: ")
+determinante3 = input("Determinante: ")
+sustantivo3 = input("Sustantivo: ")
+
+print(f"{Nombre_propio} recibió muy {adjetivo1} su regalo de cumpleaños. {Pronombre} se esperaba que fuera {determinante1} {sustantivo1}  o {determinante2} {sustantivo2}, pero cuando lo abrió se llevó una grata sorpresa, ¡Era {determinante3} {sustantivo3}! ")
+
+
